93de7c351b644f918c9ee0460018b5d604fa1e36
Paraizs Attila <paraizsattila97@gmail.com>
2024-10-30 13:34:20 +0100

    created functions and validation files

---
+++ Funkciok/Funkciok.py
@@ -0,0 +1,10 @@
+import Osztalyok
+
+def JegyFoglalas():
+    pass
+
+def FoglalasLemondasa():
+    pass
+
+def FoglalasokListazasa():
+    pass
---
+++ Osztalyok/Jarat.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class Jarat(ABC):
+    jaratszam: int
+    celallomas: str
+    jegyar: float
